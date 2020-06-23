eb7f05bc48e3e92c3bef7a54b2fa6096b2fd38a7
kaneplusplus <kaneplusplus@gmail.com>
2020-06-23 17:59:36 -0400

    Moved the directory.

--- inst/jss/kane-listdown/clean.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-rm *.aux 
-rm *.bbl 
-rm *.blg 
-rm *.log 
-rm *.out 
-rm *.pdf 
-rm -rf kane-listdown_files 
--- inst/jss/kane-listdown/supplemental-material/clean.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-rm *.rds 
-rm *.pdf 
-rm *.tex 
-rm *.html 
-rm -rf figure 
-rm trial-report.rmd
