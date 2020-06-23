eabb949e238f56e166714cf02eabc7b1502e0976
kaneplusplus <kaneplusplus@gmail.com>
2020-06-23 18:00:48 -0400

    And back.

---
+++ inst/jss/clean.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+rm *.aux 
+rm *.bbl 
+rm *.blg 
+rm *.log 
+rm *.out 
+rm -rf kane-listdown_files 
---
+++ inst/jss/supplemental-material/clean.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+rm *.rds 
+rm *.pdf 
+rm *.tex 
+rm *.html 
+rm -rf figure 
+rm trial-report.rmd
